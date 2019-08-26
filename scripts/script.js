1c7c68cbffe9ca8989a982cf1b09bb01f8462c60
Brendan Cowley <b.cowley1220@gmail.com>
2019-08-25 22:48:57 -0400

    got the scoring and alert/confirm to work. Added todo at bottom

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,5 @@
 "use strict";
 
-//! Query Selectors
-let wrapper = document.querySelector("#wrapper");
-let p1TC = document.querySelector("#p1TC");
-let p2TC = document.querySelector("#p2TC");
-// ! Event Listeners
-wrapper.addEventListener("click", wrapperClickHandler);
-
 // ! Variables
 let playerOne = "X";
 let playerOneMoves = [];
@@ -35,37 +28,71 @@ let winConditions = [
   [9, 5, 1]
 ];
 
-function whoWon(winConditions, array, player, playerScore) {
+function whoWon(winConditions, array, player, playerScore, playerSP) {
   console.log(winConditions[0]);
-  // console.log(array);
   for (let i = 0; i < winConditions.length; i++) {
     if (JSON.stringify(winConditions[i]) === JSON.stringify(array)) {
-      console.log(`${player} wins!`);
-      // console.log(`${player} score: ${playerScore}`);
+      alert(`${player} wins!`);
       playerScore++;
       console.log(`${player} score: ${playerScore}`);
-      return playerScore;
+      playerSP.innerHTML = `${player} Score ${playerScore}`;
+      // return playerScore;
+      confirm("play again"){
+
+      };
     }
   }
 }
 
 function playerTurns() {
   if (playerOneTurnCount <= playerTwoTurnCount) {
-    event.target.innerHTML = playerOne;
-    playerOneTurnCount++;
-    p1TC.innerHTML = `PlayerOne Turns: ${playerOneTurnCount}`;
-    playerOneMoves.push(parseInt(event.target.id, 10));
-    whoWon(winConditions, playerOneMoves, playerOne, playerOneScore);
+    if (event.target.classList.contains("clicked")) {
+      alert("try again wanker");
+    } else {
+      event.target.innerHTML = playerOne;
+      event.target.classList.add("clicked");
+      playerOneTurnCount++;
+      p1TC.innerHTML = `PlayerOne Turns: ${playerOneTurnCount}`;
+      playerOneMoves.push(parseInt(event.target.id, 10));
+      whoWon(winConditions, playerOneMoves, playerOne, playerOneScore, p1S);
+    }
   } else {
-    event.target.innerHTML = playerTwo;
-    playerTwoMoves.push(parseInt(event.target.id, 10));
-    playerTwoTurnCount++;
-    p2TC.innerHTML = `PlayerTwo Turns: ${playerTwoTurnCount}`;
-    whoWon(winConditions, playerTwoMoves, playerTwo, playerTwoScore);
+    console.log(event.target.id);
+    if (event.target.classList.contains("clicked")) {
+      alert("try again wanker");
+    } else {
+      event.target.innerHTML = playerTwo;
+      event.target.classList.add("clicked");
+      playerTwoTurnCount++;
+      p2TC.innerHTML = `PlayerTwo Turns: ${playerTwoTurnCount}`;
+      playerTwoMoves.push(parseInt(event.target.id, 10));
+      whoWon(winConditions, playerTwoMoves, playerTwo, playerTwoScore, p2S);
+    }
   }
 }
 
+//! Query Selectors
+let wrapper = document.querySelector("#wrapper");
+let p1TC = document.querySelector("#p1TC");
+let p2TC = document.querySelector("#p2TC");
+let p1S = document.querySelector("#p1S");
+let p2S = document.querySelector("#p2S");
+let clicked = document.querySelectorAll(".clicked");
+let boxes = document.querySelectorAll(".box");
+document
+  .querySelectorAll(".box")
+  .forEach(box => box.addEventListener("click", boxesClickHandler));
+// ! Event Listeners
+
 // ! Click Handlers
-function wrapperClickHandler(event) {
-  playerTurns(event)''
+
+function boxesClickHandler(event) {
+  event.preventDefault();
+  console.log("boxes are working");
+  playerTurns(event);
 }
+
+//Todo:
+//! Finish confirm function and reset the game board
+//! Set up remove event listener on completion of game
+//! Add in a tie option
